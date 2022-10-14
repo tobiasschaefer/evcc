f7b6d984d60da813720eb7440f7cf318e8b5681d
andig <cpuidle@gmx.de>
2022-10-14 15:37:29 +0200

    Add CachedTokenSource

--- util/oauth/cache.go
+++ util/oauth/cache.go
@@ -1,16 +1,28 @@
 package oauth
 
-import "golang.org/x/oauth2"
+import (
+	"github.com/evcc-io/evcc/api/store"
+	"golang.org/x/oauth2"
+)
 
-var _ oauth2.TokenSource = (*CachedTokenSource)(nil)
+var _ oauth2.TokenSource = (*cachedTokenSource)(nil)
 
-type CachedTokenSource struct {
-	ts oauth2.TokenSource
+type cachedTokenSource struct {
+	ts    oauth2.TokenSource
+	store store.Store
 }
 
-func (ts *CachedTokenSource) Token() (*oauth2.Token, error) {
+func CachedTokenSource(store store.Store, ts oauth2.TokenSource) oauth2.TokenSource {
+	return &cachedTokenSource{
+		ts:    ts,
+		store: store,
+	}
+}
+
+func (ts *cachedTokenSource) Token() (*oauth2.Token, error) {
 	token, err := ts.ts.Token()
-	// if err == nil &&ts.store!=nil{
-	// }
+	if err == nil && ts.store != nil {
+		err = ts.store.Save(token)
+	}
 	return token, err
 }
